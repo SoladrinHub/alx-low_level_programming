74ed21f7d4338a43f77f34bd967c4735c257c5e4
SoladrinHub <cointoken8021@gmail.com>
2023-09-14 19:11:22 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 - 9
+ *
+ * Return: numbers
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
