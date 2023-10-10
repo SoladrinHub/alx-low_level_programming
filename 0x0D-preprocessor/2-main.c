3f5244400d119f960406b9ff9a59616dba1d196b
SoladrinHub <cointoken8021@gmail.com>
2023-10-10 12:50:30 -0700

    Write a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print program name
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("This program was compiled from the file : %s\n", __FILE__);
+	return (0);
+}
+
