8906f99d19f6dcaea2cb0171723b8bb202551830
SoladrinHub <cointoken8021@gmail.com>
2023-10-11 03:13:49 -0700

    Define a new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type of struct
+ * @name: first field
+ * @age: second field
+ * @owner: third field
+ * Description: longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
